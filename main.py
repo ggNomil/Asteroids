0fef42ebea1e82651eb63f76777525408dd0d371
ggNomil <alimon927@gmail.com>
2025-04-01 19:57:27 -0500

    Adding player drawing and controls

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
         screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        # limit game to 60fps
+        dt = clock.tick(60) / 1000.0
+
+
 
 
 
